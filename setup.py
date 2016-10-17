655f84c4db2da933815245009371aa8e3fc61c92
Guilherme Diego <guidiego.expgames@gmail.com>
2016-10-17 20:09:04 -0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+m distutils.core import setup
+
+setup(
+    name='pyinterpreter',
+    version='0.0.1',
+    description='Python Interpreter in background',
+    author='Rafael Cassau',
+    author_email='rafa.cassau@gmail.com',
+    url='',
+    packages=[],
+)
