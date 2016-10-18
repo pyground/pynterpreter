a00d692f50e42567a55182415c5ea60508552f8a
Guilherme Diego <guidiego.expgames@gmail.com>
2016-10-17 22:23:55 -0200

    Upload core module

---
+++ core/test_pynterpreter.py
@@ -0,0 +1,10 @@
+"""
+def test_check_integraty_to_be_false():
+def test_check_integraty_to_be_false():
+def test_clean_stdout():
+def test_clean_stdout_with_none_on_param():
+def test_clean_stderr():
+def test_clean_stderr_with_none_on_param():
+def test_run_as_module():
+def test_run_as_module_withou_module():
+"""
